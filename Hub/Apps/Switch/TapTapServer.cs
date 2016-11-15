0725acbcbe634f9825b2f6c46e233354c08b85e0
jhartman <jhartman@sfu.ca>
2016-11-15 00:20:56 -0500

    Add system classes

--- Hub/Apps/Switch/TapTapServer.cs
+++ Hub/Apps/Switch/TapTapServer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace HomeOS.Hub.Apps.Switch
 {
