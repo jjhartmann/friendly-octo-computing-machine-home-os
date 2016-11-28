b59cc5f39a4ee28f95dcc20f06754f15f0f8fb3e
jhartman <jhartman@sfu.ca>
2016-11-28 12:43:54 -0500

    Add constant class for TapTap

---
+++ Hub/Apps/TapTap/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeOS.Hub.Apps.TapTap
+{
+    static class TapTapConstants
+    {
+        public const bool DEBUG_CLIENT_SERVER = true;
+        public const string STR_NULL = "NULL";
+        public const string CERT_DIR_FILE = "\\Cert\\certificate.cer";
+        public const string CONFIG_DIR = "\\Config";
+        public const string CONFIG_FILE = "taptapconfig.xml";
+
+    }
+
+    static class ENUM_MESSAGE_TYPE
+    {
+        public const string ACTION_BINARY_SWITCH = "binarySwitch";
+        public const string ACTION_ADD_DEVICE_REQUEST = "addDeviceRequest";
+    }
+}
